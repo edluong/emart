40d1fca99b1cfa3116a5b9f9a968af22d70909dc
edluong <edmundluong@gmail.com>
2018-07-30 21:29:09 -0700

    first commit

---
+++ iprodtbl.sql
@@ -0,0 +1,20 @@
+
+
+begin;
+
+insert into product(
+	prodname,
+	price
+)
+
+values 
+('book',20),
+('wallet',20.98),
+('camera',199),
+('laptop',700.99),
+('smartphone',800.83),
+('video game console',340.84)
+
+--commit;
+--rollback;
+
